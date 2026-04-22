5e082cb61e9cef78234bfd2de09266b3b27cd321
“[Vidura <Vidura1159@gmail.com>
2026-04-22 10:30:59 +0530

    fix prisma errors and edit env.example file

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+}
